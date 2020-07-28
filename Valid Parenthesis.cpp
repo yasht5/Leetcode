5b54010fa89f0c2494fb315f0da4af2339af7fba
Yash Toshniwal <65651244+yasht5@users.noreply.github.com>
2020-07-29 00:25:08 +0530

    Create Valid Parenthesis.cpp

---
+++ Valid Parenthesis.cpp
@@ -0,0 +1,37 @@
+class Solution{
+public:
+	bool isValid(string s){
+		stack<char> st;
+		char x;
+		for(int i=0;i<s.length();i++){
+			char c =s[i];
+			if(s[i]=='(' || s[i]=='{' || s[i]=='[')
+				st.push(s[i]);
+				continue;
+			}
+			if(st.empty())
+				return false;
+			switch(s[i]){
+				case ')':
+					x=st.top();
+					st.pop();
+					if(x=='{' || x=='[')
+						return false;
+					break;
+				case '}':
+					x=st.top();
+					st.pop();
+					if(x=='[' || x=='(')
+						return false;
+					break;
+				case ']':
+					x=st.top();
+					st.pop();
+					if(x=='(' || x=='{')
+						return false;
+					break;
+			}
+		}
+		return st.empty();
+	}
+};

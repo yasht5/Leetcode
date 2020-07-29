6fff3320754f8ea33ef794be5561fbb1f788785e
Yash Toshniwal <65651244+yasht5@users.noreply.github.com>
2020-07-30 00:29:35 +0530

    Update Implement strStr().cpp

--- Implement strStr().cpp
+++ Implement strStr().cpp
@@ -1,8 +1,10 @@
 class Solution{
 public:
 	int strStr(string A, string B){
+		
 		int hs=A.size();
 		int nz=B.size();
+		if(!nz)	return 0;
 		int start=0, tempstart=0;
 		int j;
 		while(tempstart<hs && j<nz){

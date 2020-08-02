2bd77ea7264263626195fbc7ffc64527d54c58f1
Yash Toshniwal <65651244+yasht5@users.noreply.github.com>
2020-08-02 17:08:41 +0530

    Create Merge Interval.cpp

---
+++ Merge Interval.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    
+    vector<vector<int>> merge(vector<vector<int>>& A) {
+        vector<vector<int>> res;
+        if(A.size()==0)
+            return res;
+        sort(A.begin(), A.end());
+        res.push_back(A[0]);
+        for(int i=1;i<A.size();i++){
+            if(A[i][0]<=res[res.size()-1][1])
+                res[res.size()-1][1] = max(res[res.size()-1][1], A[i][1]);
+            else
+                res.push_back(A[i]);
+        }
+        return res;
+    }
+};

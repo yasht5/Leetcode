0da98136f5c6fc75f481a40884a8749c9b68369a
Yash Toshniwal <65651244+yasht5@users.noreply.github.com>
2020-08-07 01:09:07 +0530

    Create Symmetric Tree.cpp

---
+++ Symmetric Tree.cpp
@@ -0,0 +1,25 @@
+class Solution{
+public:
+	bool isSymmteric(TreeNode* root){
+		queue<TreeNode*> q;
+		q.push(root);
+		q.push(root);
+		while(!q.empty()){
+			TreeNode* t1=q.front();
+			q.pop();
+			TreeNode* t2 = q.front();
+			q.pop();
+			if(!t1 && !t2)
+				continue;
+			if(!t1 || !t2)
+				return false;
+			if(t1->val != t2->val)
+				return false;
+			q.push(t1->left);
+			q.push(t2->right);
+			q.push(t1->right);
+			q.push(t2->left);
+		}
+		return true;
+	}
+};

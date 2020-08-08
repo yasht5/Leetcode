5e0d2322ebbc6f7219ac37ca0cd87f82c372275b
Yash Toshniwal <65651244+yasht5@users.noreply.github.com>
2020-08-08 22:29:44 +0530

    Create Minimum Insertion to balance A Parenthesis String.cpp

---
+++ Minimum Insertion to balance A Parenthesis String.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minInsertions(string s) {
+        stack<char> v;
+        int ans = 0;
+        for(int i = 0;i < s.size();i++){
+            if(s[i] == '(') v.push(s[i]);
+            else{
+                if(s[i] == ')' && i < s.size() && s[i + 1] == ')') {
+                    if(!v.empty())
+                        v.pop();
+                    else ans++;
+                    i++;       // because we considered i+1 in this case
+                }
+                else if(s[i] == ')' && i < s.size() && s[i + 1] != ')'){
+                    if(!v.empty()){
+                        v.pop();
+                        ans++;
+                    }
+                    else ans += 2;
+                }
+            }
+        }
+        if(v.empty()) return ans;
+        return v.size()*2 + ans;
+    }
+};
